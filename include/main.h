3731bcb220adda4564203733b06bbe3fa7ee6e52
song <18339512532@163.com>
2023-03-19 23:07:29 +0800

    add LeteCode No.1625

--- include/main.h
+++ include/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 extern int *twoSum(int* nums, int numsSizes, int target, int* returnSize);
-
+extern char * findLexSmallestString(char * s, int a, int b);
 
 #endif // !MAIN_H
--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 void main(void)
 {
-    int nums[]= {2,7,11,15};
-    int target = 9;
-    int size;
-    int* ret = twoSum(nums,sizeof(nums)/sizeof(nums[0]),target,&size);
+    // int nums[]= {2,7,11,15};
+    // int target = 9;
+    // int size;
+    // int* ret = twoSum(nums,sizeof(nums)/sizeof(nums[0]),target,&size);
 
-    if(size!=0) {
-        printf("%d %d\n",ret[0],ret[1]);
-    }
+    // if(size!=0) {
+    //     printf("%d %d\n",ret[0],ret[1]);
+    // }
 
+    char* s= "5525";
+    printf("res=  %s\n",findLexSmallestString(s,9,2));
     printf("build time %s,%s\n",__DATE__,__TIME__);
     return;
 }
